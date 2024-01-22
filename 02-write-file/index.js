2c853c2b533c467bba94db871597c12c53b28dbd
eansable <eansable@yandex.by>
2024-01-23 00:02:37 +0300

    feat: finished task 2

---
+++ 02-write-file/index.js
@@ -0,0 +1,23 @@
+const { log } = require('node:console');
+const fs = require('node:fs');
+const path = require('node:path');
+fs.writeFile(path.resolve(__dirname, './text.txt'), '', 'utf-8', () => {})
+const createFile = fs.createWriteStream(path.resolve(__dirname, './text.txt'))
+let textFinish = ''
+process.stdin.setEncoding('utf-8')
+console.log("Please, write text!");
+process.stdin.on('data', text => {
+    console.log(text.toString());
+    if (text.toString() === 'exit')
+        process.exit(0)
+    textFinish += text
+})
+
+process.on('SIGINT', (value) => {
+    process.exit(0)
+})
+
+process.on('exit', (text, edsa) => {
+   createFile.write(textFinish)
+   console.log("File was to write");
+})
